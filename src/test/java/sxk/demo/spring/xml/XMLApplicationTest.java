bf87aef197d427055efdfdba4ca4219c1bbcbbae
sunxikai <xikai.sun@pactera.com>
2019-08-08 18:00:32 +0800

    通过xml配置spring

---
+++ src/test/java/sxk/demo/spring/xml/XMLApplicationTest.java
@@ -0,0 +1,20 @@
+package sxk.demo.spring.xml;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import sxk.demo.spring.pojo.Person;
+
+/**
+ * Created by sunxikai on 19/8/8.
+ */
+public class XMLApplicationTest {
+    @Test
+    public void test() {
+        /*
+        通过xml配置spring
+         */
+        ClassPathXmlApplicationContext app = new ClassPathXmlApplicationContext("application.xml");
+        Person person = (Person) app.getBean("person");
+        System.out.println("姓名:" + person.getName() + ",年龄:" + person.getAge());
+    }
+}
